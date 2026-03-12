c27f877ea4b182514b699f0cd07112d73c40e04c
madik2467-art <madik2467@gmail.com>
2026-03-13 04:55:25 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,8 @@ ADMIN_ID = 5243526187
 SHEET_BEST_URL = "https://api.sheetbest.com/sheets/db56727e-6601-4c53-9163-bfcf7fa4794f"
 SHEET_BEST_KEY = "KyzgKBDQzYBxbXBWFb2Wg6wW9ZjbwW1jXi9bKvSFrG_ddW9k%Z#B7EP9xcne6J1u"
 
-# Neon Database с портом 5433 (SSL)
+# Neon Database с портом 5433 (SSL)# config.py
 DATABASE_URL = "postgresql://neondb_owner:npg_EFdcgysS3j7r@ep-weathered-band-adwsari7-pooler.c-2.us-east-1.aws.neon.tech:5433/neondb?sslmode=require"
-
 
 WEBAPP_URL = "https://tgbot-kappa-six.vercel.app/"
 
