450fea79a5aaf4d72d2069ae402811eabf3da04b
madik2467-art <madik2467@gmail.com>
2026-03-12 18:04:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 BOT_TOKEN = "7971840018:AAHbCaoN_39e4i6BCbKynDUgP5y64eZMNdw"
 GROQ_API_KEY = "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"
-ADMIN_ID = 6106034597
+ADMIN_ID = 5243526187
 SHEET_BEST_URL = "https://api.sheetbest.com/sheets/db56727e-6601-4c53-9163-bfcf7fa4794f"
 SHEET_BEST_KEY = "KyzgKBDQzYBxbXBWFb2Wg6wW9ZjbwW1jXi9bKvSFrG_ddW9k%Z#B7EP9xcne6J1u"
 
@@ -12,4 +12,5 @@ DATABASE_URL = "postgresql://neondb_owner:npg_EFdcgysS3j7r@ep-weathered-band-adw
 
 
 WEBAPP_URL = "https://tgbot-663622rf9-madik2467-arts-projects.vercel.app/"
+
 
